e4bff0b9b369367cebfc7bd5526097315f6c0409
questforcode <iulianasb@gmail.com>
2019-04-29 10:47:24 +0200

    fibonacci

--- Javascript/16_fibonacci.js
+++ Javascript/16_fibonacci.js
@@ -1,6 +1,8 @@
 /*
  * Program that presents the Fibonacci series
  * up to a certain number.
+ * This program just gives me a specific number of things in the series. Bottom exercise
+ * is the 'correct' way to do it.
  */
 
 /**
@@ -34,4 +36,78 @@ function show(num) {
     console.log(fibonacci(num))
 }
 
-show(20)
+let num = 10
+show(num)
+
+
+/*-------------------------------------------------------------------*/
+
+
+/**
+ * Class Version of This
+ */
+
+/**
+ * function fiboClass
+ * @param limit: number
+ * @returns : array [numbers]
+ */
+function fiboClass(limit) {
+    let i = 0
+    let j = 1
+    let group = [i]
+
+    //Not recommended for, because we have no idea of the limit.
+    /*    for (let k = 0; k < Infinity; k++) {
+           const element = array[k];
+           if() {
+               //continue
+               break //exit condition to stop an infinite loop.
+           } */
+
+    while (j <= limit) {
+        let next = i + j
+        i = j
+        j = next
+        group.push(i)
+
+
+    }
+    return group
+}
+
+let limit = 200
+console.log(fiboClass(limit))
+
+
+
+/**
+ *  Class For Version. Not recommended.
+ * */
+
+function fiboFor(limit2) {
+    /*    let i = 0
+       let j = 1 */
+    let group2 = [0]
+
+    /*     for (let ind = 0; ind < Infinity; ind++) {
+            let next = i + j
+            i = j
+            j = next
+            group2.push(i)
+            if (j > limit2) {
+                break
+            }
+     */
+
+    for (let i = 0, j = 1; i < limit2; i = j, j = next) {
+        group2.push[j]
+        next = i + j
+
+    }
+    return group2
+}
+
+
+let limit2 = 30
+console.log(fiboFor(limit2))
