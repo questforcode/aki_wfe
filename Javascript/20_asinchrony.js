4c0216d677a75a9d504dcd1223540e13c78cad21
questforcode <iulianasb@gmail.com>
2019-04-29 13:34:31 +0200

    asinchrony

---
+++ Javascript/20_asinchrony.js
@@ -0,0 +1,19 @@
+/**
+ * The function setTimeout
+ * allows for an asynchronous process, out of time.
+ */
+
+ console.log('First')
+
+ //A while from now, console.log'second', second would come up last.
+ //Timeout is read in miliseconds.
+ 
+setTimeout ( () => {
+    console.log('Second')
+}, 1000)
+
+ console.log('Third')
+
+ //The difference between server and client time is what creates asynchrony. We have to work on it.
+
+ //This is the end of JS Core.
