3d5fa87d5dd21e9d37f4e1996cc8fa43de648a61
questforcode <iulianasb@gmail.com>
2019-05-07 12:31:24 +0200

    arrays

--- Javascript/29_arrays.js
+++ Javascript/29_arrays.js
@@ -26,3 +26,31 @@ function randoms(limit = 1) {
 }
 
 console.log(randoms(12))
+
+
+//----// Learning arrays function methods.
+
+function test(params) {
+    let data = [1, 7, 3, 2]
+    let moreData = [1, 4, 6]
+    let final = data.concat(moreData) //concat returns a new array, doesn't modify the original array
+    //data.sort() //modifies the array.
+
+    // temp = data creates a new reference to the same object, so modifying it would also modify data. we cannot
+    // have both the original and the sorted that way.
+    //This is where we use array cloning with slice and splice.
+
+    //slice takes a bit of array, clones it into a new array, and returns it.
+    let temp = data.slice(0)
+    temp.sort()
+
+    //sort can have an alternative. use a callback to tell it how to order.
+    data2 = [1, 7, 70, 3, 9]
+    data2.sort(a,b => a - b) //ordering function)
+
+    console.log(+data.join(''))
+    console.log(temp)
+    console.log(final)
+    console.log(data2)
+}
+test()
