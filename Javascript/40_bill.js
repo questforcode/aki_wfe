343d599c64fc01e28939fedc62ffff004813b1e7
questforcode <iulianasb@gmail.com>
2019-05-09 14:03:50 +0200

    bill

---
+++ Javascript/40_bill.js
@@ -0,0 +1,37 @@
+let bill = {
+    company: {
+        name: '',
+        address: '',
+        phone: '',
+        id: ''
+    },
+    client: {
+        name: '',
+        address: '',
+        phone: '',
+        id: '',
+    },
+    items: [
+        {description: '', price: 20, quantity: 12},
+        {description: '', price: 24, quantity: 10},
+        {description: '', price: 34, quantity: 7}
+    ],
+    total: 0,
+    taxType: 0.23,
+    paymentMethod: 'Cash',
+    calculateTotal: function () {
+        let tax = this.taxType < 1 ? 1+this.taxType : this.taxType
+        console.log(tax)
+        return this.items.map(item => item.price * item.quantity).reduce((a,b) =>a+b)*tax
+    },
+    refreshTotal: function () {
+        this.total = this.calculateTotal()
+    },
+    showTotal: function () {
+        this.refreshTotal()
+        let msg = `The total price with tax included is ` + this.total + ` Euros.`
+        console.log(msg)
+    }
+}
+
+bill.showTotal()
