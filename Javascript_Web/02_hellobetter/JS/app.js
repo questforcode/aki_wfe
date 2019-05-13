ebaca58baa78931ef03ad88f82f34a6ffa83d1be
questforcode <iulianasb@gmail.com>
2019-05-13 13:58:01 +0200

    hello better

--- Javascript_Web/02_hellobetter/JS/app.js
+++ Javascript_Web/02_hellobetter/JS/app.js
@@ -1,17 +1,27 @@
-//Event fundling function
-function clickMe() {
 
-    //Event handling function
+function clickMe() {
     console.log('Falling for these things over and over says much about you.')
 }
-function goAway () {
+function goAway() {
     console.log('This stubborness makes absolutely no sense.')
 }
 
-function onMouseOver () {
+function onMouseOver() {
     console.log('The mouse is in!')
 }
 
-function onMouseOut () {
+function onMouseOut() {
     console.log('The mouse is out!')
+}
+
+/**
+ * Accessing the DOM (Document Object Model). JS reads documents as objects. 
+ */
+
+window.onload = function () {
+    document.querySelector('#button-yes').onclick = clickMe
+    //document.querySelector('button:nth-of-type(2)').onclick = goAway
+    document.querySelector('#button-no').onclick = goAway
+    document.querySelector('#mouse-div').onmouseover = onMouseOver
+    document.querySelector('#mouse-div').onmouseout = onMouseOut
 }
