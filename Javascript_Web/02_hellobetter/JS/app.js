e6552b43e35840afc6ae4cc8b80af7990ecf0aec
questforcode <iulianasb@gmail.com>
2019-05-13 13:13:02 +0200

    hello better

---
+++ Javascript_Web/02_hellobetter/JS/app.js
@@ -0,0 +1,17 @@
+//Event fundling function
+function clickMe() {
+
+    //Event handling function
+    console.log('Falling for these things over and over says much about you.')
+}
+function goAway () {
+    console.log('This stubborness makes absolutely no sense.')
+}
+
+function onMouseOver () {
+    console.log('The mouse is in!')
+}
+
+function onMouseOut () {
+    console.log('The mouse is out!')
+}
