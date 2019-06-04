5e10ab5e65a10be3dde7486ee3b5eabf128c039d
questforcode <iulianasb@gmail.com>
2019-06-04 11:55:45 +0200

    sociallinks

--- Javascript_Web/14_sociallinks/JS/controller.js
+++ Javascript_Web/14_sociallinks/JS/controller.js
@@ -1,3 +1,19 @@
 export function controller() {
-    
+    //doms
+    const aNodesLinks = document.querySelectorAll('.social-menu a')
+    const clicker = document.querySelector('#clicker')
+    //event handler defined
+
+    aNodesLinks.forEach( item => item.addEventListener('click', onClick))
+    clicker.addEventListener('click', clickLink)
+
+    //event handler implemented
+
+    function onClick (ev) {
+        ev.target.outerHTML += ` Oops, ${ev.target.title} not exist! <br>` //inner adds, outer substitutes
+    }
+
+    function clickLink () {
+        console.log(`You've clicked it!`)
+    }
 }
