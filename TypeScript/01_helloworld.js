f8eb06114eb5fecb27dd23997c29a31de042d88b
questforcode <iulianasb@gmail.com>
2019-06-17 13:12:34 +0200

    helloworld

---
+++ TypeScript/01_helloworld.js
@@ -0,0 +1,5 @@
+{
+    var string = 'Hello, World!';
+    console.log(string);
+}
+//compile the file into JS by writing tsc in console
---
+++ TypeScript/01_helloworld.ts
@@ -0,0 +1,10 @@
+{
+    let string: string
+    string = 'Hello, World!'
+    //string = 23 | typescript lets you know when you make errors and will let you know what compliling
+    console.log(string)
+}
+
+
+//compile the file into JS by writing tsc in console
+//compiles in ES5 by default
