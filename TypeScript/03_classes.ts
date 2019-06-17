dc79bbc2c2b0d47c0b52cddbd259a4eff8c31c42
questforcode <iulianasb@gmail.com>
2019-06-17 14:01:37 +0200

    classes

---
+++ TypeScript/03_classes.ts
@@ -0,0 +1,38 @@
+class Person { 
+    name
+    age
+    food
+    constructor(name, age, food) { 
+        this.name = name
+        this.age = age
+        this.food = food
+        // this.soul = true
+    }
+    salute(toWhom = 'friend') { 
+
+        console.log(`Hello, ${toWhom}, I'm ${this.name}.`)
+    }
+}
+
+let person1 = new Person('Rayne', 20, 'Honey')
+
+// person1.height = 149 it would give an error in TS but not in JS so it's fine.
+
+console.log(person1)
+person1.salute()
+
+class Student extends Person {
+    course
+ constructor (name, age, food, course) {
+     super(name, age, food)
+     this.course = course
+ }
+ salute(toWhom = 'friend') {
+     super.salute(toWhom)
+     console.log(`I am studying ${this.course}.`)
+ }
+}
+
+let student1 = new Student ('Vulkan', 25, 'Roast', 'Tank Driving')
+console.log(student1)
+student1.salute()
